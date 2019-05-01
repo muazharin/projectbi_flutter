9a770528d2a43e4a6a2d812071cd4eb82ee391b1
Muaz <muaz@localhost.localdomain>
2019-05-01 23:16:58 +0800

    halaman desk

--- lib/deskPage.dart
+++ lib/deskPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:simple_permissions/simple_permissions.dart';
+import 'package:http/http.dart' as http;
 
 class DeskPage extends StatefulWidget {
   //@override
@@ -31,8 +32,40 @@ class _DeskPageState extends State<DeskPage> {
         child: new Container(
           child: Center(
             child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                new Text('$_barcodeString'+this.username)
+                new Text('$_barcodeString'),
+                RaisedButton(
+                  onPressed: (){
+                    // Navigator.pushReplacementNamed(context, '/login');
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        // return object of type Dialog
+                        return AlertDialog(
+                          title: new Text("Exit"),
+                          content: new Text("Are you really want to exit?"),
+                          actions: <Widget>[
+                            // usually buttons at the bottom of the dialog
+                            new FlatButton(
+                              child: new Text("Cancel"),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            new FlatButton(
+                              child: new Text("Yes"),
+                              onPressed: () {
+                                Navigator.of(context).pushNamedAndRemoveUntil('/login', (Route<dynamic> route) => false);
+                              },
+                            )
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: Text('Exit'),
+                )
               ],
             ),
           ),
@@ -48,11 +81,22 @@ class _DeskPageState extends State<DeskPage> {
     bool result = (await SimplePermissions.requestPermission(permission)) as bool;
   }
 
+  void input(String _barcodeString){
+    var url = "http://192.168.2.19/project_bi/kegiatan/addData";
+
+    http.post(url, body: {
+      "tabel": _barcodeString,
+      "username": this.username,
+    });
+  }
+
   scan() async{
     try{
       String reader = await BarcodeScanner.scan();
       if(!mounted){return;}
       setState(() => _barcodeString = reader);
+      input(_barcodeString);
+      //Navigator.pop(context);
     }on PlatformException catch(e){
       if(e.code==BarcodeScanner.CameraAccessDenied){
         requestPermission();
